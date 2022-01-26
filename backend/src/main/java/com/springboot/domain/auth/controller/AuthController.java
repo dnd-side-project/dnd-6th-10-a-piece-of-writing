cf764472771b460f05b661e15a22a97ef33deb60
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-01-26 16:59:33 +0900

    [feat] 로그인 로직 수정

--- backend/src/main/java/com/springboot/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/springboot/domain/auth/controller/AuthController.java
@@ -1,11 +1,19 @@
 package com.springboot.domain.auth.controller;
 
+import com.springboot.domain.auth.jwt.JwtUtil;
+import com.springboot.domain.common.error.exception.ErrorCode;
+import com.springboot.domain.common.error.exception.InvalidValueException;
 import com.springboot.domain.common.model.RequestDto;
+import com.springboot.domain.common.model.ResponseDto;
+import com.springboot.domain.common.model.ResponseService;
+import com.springboot.domain.common.model.SuccessCode;
+import com.springboot.domain.member.model.Member;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import com.springboot.domain.member.repository.MemberRepository;
 import com.springboot.domain.member.service.MemberService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
-
-    private final MemberRepository memberRepository;
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+    private final ResponseService responseService;
 
     @PostMapping(value = "/login")
-    public ResponseEntity login(@RequestBody RequestDto requestDto) {
-        String body = memberService.login(requestDto.getEmail(), requestDto.getPassword());
+    public ResponseEntity<ResponseDto> login(@RequestBody RequestDto requestDto) {
+        Member member = (Member) memberService.loadUserByUsername(requestDto.getEmail());
 
-        return ResponseEntity.status(HttpStatus.OK).body(body);
+        if(!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) {
+            throw new InvalidValueException(requestDto.getPassword(), ErrorCode.PASSWORD_INVALID);
+        }
+        String token = JwtUtil.createAuthToken(requestDto.getEmail());
+        Map<String, String> body = new HashMap<>();
+        body.put("access-token", token);
+
+        return responseService.successResult(SuccessCode.LOGIN_SUCCESS, body);
     }
 }
--- backend/src/main/java/com/springboot/domain/member/service/MemberService.java
+++ backend/src/main/java/com/springboot/domain/member/service/MemberService.java
@@ -1,12 +1,12 @@
 package com.springboot.domain.member.service;
 
+import com.springboot.domain.common.error.exception.EntityNotFoundException;
+import com.springboot.domain.common.error.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import com.springboot.domain.auth.jwt.JwtUtil;
 import com.springboot.domain.member.model.Member;
 import com.springboot.domain.member.repository.MemberRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,21 +18,12 @@ public class MemberService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws EntityNotFoundException {
         return memberRepository.findMemberByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException(email));
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
     }
 
     public Member save(Member member) {
         return memberRepository.save(member);
-    }
-
-    public String login(String email, String password) {
-        Member member = (Member) loadUserByUsername(email);
-
-        if( passwordEncoder.matches(password, member.getPassword())) {
-            return JwtUtil.createAuthToken(email);
-        }
-        return null;
     }
 }
