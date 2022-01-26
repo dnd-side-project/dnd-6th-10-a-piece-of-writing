1d39eca3527b36757ffea7aa098f5f5147d8c282
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-01-26 12:24:04 +0900

    [feat] 응답 객체 수정
    응답 객체 static 생성 함수 추가

--- backend/src/main/java/com/springboot/domain/common/model/ResponseDto.java
+++ backend/src/main/java/com/springboot/domain/common/model/ResponseDto.java
@@ -5,12 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class ResponseDto {
+    private int status;
     private String message;
     private Object data;
+
+    public static ResponseDto commonResult() {
+        return new ResponseDto(200, "request success", new Object());
+    }
+    public static ResponseDto commonResult(Object object) {
+        return new ResponseDto(200, "request success", object);
+    }
 }
