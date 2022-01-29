fde67a91ddc7289258d08539f9e9da9a0a399171
carnival77 <stam0325@gmail.com>
2022-01-30 00:11:47 +0900

    [feat] 무중단 배포 체크 위한 버젼 반환 메소드 테스트 코드 추가  #27

--- backend/src/test/java/com/springboot/domain/hello/controller/HelloControllerTest.java
+++ backend/src/test/java/com/springboot/domain/hello/controller/HelloControllerTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +28,9 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
 //    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
@@ -34,6 +39,12 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    void name() {
+        /* then */
+        assertDoesNotThrow(() -> applicationContext.getBean(HelloController.class));
     }
 
 }
