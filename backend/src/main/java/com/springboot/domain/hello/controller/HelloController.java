4e7eb1295c70e1c6a954cceaa9b589dad5251e44
carnival77 <stam0325@gmail.com>
2022-01-29 23:56:49 +0900

    [feat] 무중단 배포 체크 위한 버젼 반환 메소드 컨트롤러에 추가  #27

--- backend/src/main/java/com/springboot/domain/hello/controller/HelloController.java
+++ backend/src/main/java/com/springboot/domain/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.springboot.domain.hello.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${logging-module.version}")
+    private String version;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
     @GetMapping("/")
-    public String hello2() {
-        return "hello!!";
+    public String version() {
+        return String.format("Project Version : %s", version);
     }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+
+//    @GetMapping("/")
+//    public String hello2() {
+//        return "hello!!";
+//    }
 
 //    @GetMapping("/hello/dto")
 //    public HelloResponseDto helloDto(@RequestParam("name") String name,
