cc5a26b8174c3f065da03fb270f29005dec5b219
carnival77 <stam0325@gmail.com>
2022-02-02 23:32:11 +0900

    [feat] CI/CD 파이프라인 업그레이드 - profile : real1, real2 활용 port switching - 테스트 커밋 #27

--- backend/scripts/run_new_was.sh
+++ backend/scripts/run_new_was.sh
@@ -35,3 +35,5 @@ nohup java -jar \
     -Dserver.port=${TARGET_PORT} /home/ec2-user/dnd-6th-10-a-piece-of-writing/backend/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
+
+##
