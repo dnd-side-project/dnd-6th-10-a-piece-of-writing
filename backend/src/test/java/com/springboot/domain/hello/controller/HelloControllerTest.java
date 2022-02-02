e10a2bb3eedd2bc0964ff1a433dda1b1783626dc
carnival77 <stam0325@gmail.com>
2022-02-02 17:49:47 +0900

    [feat] CI/CD 파이프라인 업그레이드 - profile : real1, real2 활용 port switching - HelloControllerTest security 적용  #27

--- backend/src/test/java/com/springboot/domain/hello/controller/HelloControllerTest.java
+++ backend/src/test/java/com/springboot/domain/hello/controller/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.springboot.domain.hello.controller;
 
+import com.springboot.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +18,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-//@WebMvcTest(controllers = HelloController.class,
-//        excludeFilters = {
-//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
-//        }
-//)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
+//@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
     @Autowired
@@ -31,7 +32,7 @@ public class HelloControllerTest {
     @Autowired
     private ApplicationContext applicationContext;
 
-//    @WithMockUser(roles="USER")
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
