756be1b85d711887e1d14cb3e0debe954f2624b0
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-04 23:00:53 +0900

    [feat] Dto 추가

--- backend/src/main/java/com/springboot/domain/member/model/MemberDto.java
+++ backend/src/main/java/com/springboot/domain/member/model/MemberDto.java
@@ -1,4 +1,4 @@
-package com.springboot.domain.common.model;
+package com.springboot.domain.member.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RequestDto {
+public class MemberDto {
 
     private Long memberId;
     private String nickname;
