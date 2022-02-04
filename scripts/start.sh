36b6fbd4423394588762036eb20024191bfd0489
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-05 03:56:40 +0900

    [feat] 실행부분 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,8 +6,10 @@ ROOT_DIR=$(dirname $SCRIPT_DIR)
 
 if test -f "$ROOT_DIR/is-frontend-changed"; then
     chmod +x $SCRIPT_DIR/frontend-start.sh
+    bash $SCRIPT_DIR/frontend-start.sh
 fi
 
 if test -f "$ROOT_DIR/is-backend-changed"; then
     chmod +x $SCRIPT_DIR/backend-start.sh
+    bash $SCRIPT_DIR/backend-start.sh
 fi
