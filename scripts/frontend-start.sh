63c9aee1f33dc25ecc6ecffab8e7dbfd5cb1cb79
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-05 20:44:02 +0900

    [feat] fe 에 모듈 install 로직 추가

--- scripts/frontend-start.sh
+++ scripts/frontend-start.sh
@@ -11,6 +11,10 @@ echo "> 기존 프로세스 중단"
 echo "> pm2 kill"
 pm2 kill
 
+echo "> 빌드된 경로로 이동"
+echo "> cd $REPOSITORY/$PROJECT_DIR"
+cd $REPOSITORY/$PROJECT_DIR && yarn install
+
 echo "> 새 어플리케이션 배포"
 echo "> pm2 start pm2.config.js"
 pm2 start $REPOSITORY/$PROJECT_DIR/pm2.config.js
