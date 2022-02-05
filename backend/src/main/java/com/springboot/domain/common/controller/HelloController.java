dedb643a30bed8ab96ed71beab53f385f807a22c
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-05 16:21:18 +0900

    [chore] 패키지 이동

--- backend/src/main/java/com/springboot/domain/common/controller/HelloController.java
+++ backend/src/main/java/com/springboot/domain/common/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.springboot.domain.hello.controller;
+package com.springboot.domain.common.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
