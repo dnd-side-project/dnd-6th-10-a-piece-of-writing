dfe2a191aae96d8602b1f83e4d90aff591bb4ba4
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-05 20:31:20 +0900

    [fest] fe test

--- scripts/frontend-start.sh
+++ scripts/frontend-start.sh
@@ -3,9 +3,9 @@
 REPOSITORY=/home/ec2-user
 PROJECT_DIR=dnd-6th-10-a-piece-of-writing/frontend
 
-#echo "> 빌드된 경로로 이동"
-#echo "> cd $REPOSITORY/$PROJECT_DIR"
-#cd $REPOSITORY/$PROJECT_DIR
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo "> 기존 프로세스 중단"
 echo "> pm2 kill"
