bfef9e942b35d556dcd4a37d0e68c5116811eea0
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-05 20:24:23 +0900

    [test] fe test

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,10 +6,12 @@ ROOT_DIR=$(dirname $SCRIPT_DIR)
 
 if test -f "$ROOT_DIR/is-frontend-changed"; then
     chmod +x $SCRIPT_DIR/frontend-start.sh
+    echo "> Execute frontend-start.sh"
     bash $SCRIPT_DIR/frontend-start.sh
 fi
 
 if test -f "$ROOT_DIR/is-backend-changed"; then
     chmod +x $SCRIPT_DIR/backend-start.sh
+    echo "> Execute backend-start.sh"
     bash $SCRIPT_DIR/backend-start.sh
 fi
