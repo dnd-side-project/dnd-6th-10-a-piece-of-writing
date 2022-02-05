213f7b918a3949fa0f9bdf5a34b0584b905b4d5f
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-05 20:05:45 +0900

    [test] frontend 배포 테스트

--- scripts/frontend-start.sh
+++ scripts/frontend-start.sh
@@ -3,9 +3,9 @@
 REPOSITORY=/home/ec2-user
 PROJECT_DIR=dnd-6th-10-a-piece-of-writing/frontend
 
-echo "> 빌드된 경로로 이동"
-echo "> cd $REPOSITORY/$PROJECT_DIR"
-cd $REPOSITORY/$PROJECT_DIR
+#echo "> 빌드된 경로로 이동"
+#echo "> cd $REPOSITORY/$PROJECT_DIR"
+#cd $REPOSITORY/$PROJECT_DIR
 
 echo "> 기존 프로세스 중단"
 echo "> pm2 kill"
@@ -13,4 +13,4 @@ pm2 kill
 
 echo "> 새 어플리케이션 배포"
 echo "> pm2 start pm2.config.js"
-pm2 start pm2.config.js
+pm2 start $REPOSITORY/$PROJECT_DIR/pm2.config.js
