ab532bc656b9178bf6b0c8f8ee0153b6b61d0b32
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-06 20:54:36 +0900

    [feat] tailwind 설정 변경

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      h1: ['54px'],
+      h2: ['32px'],
+      h3: ['24px'],
+      h4: ['21px'],
+      subtitle: ['24px'],
+      button: ['16px'],
+      link: ['16px'],
+      overline: ['14px'],
+    },
   },
   plugins: [],
 }
