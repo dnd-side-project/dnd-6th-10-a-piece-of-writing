7b0ea1660b6a29324a042368524d4bbebc2240aa
서상혁[쇼핑서비스FE] <sang-hyeok.seo@navercorp.com>
2022-02-06 20:53:42 +0900

    [feat] styled-component 설정 변경

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,9 +1,34 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class WebAppDocument extends Document {
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
   render() {
     return (
-      <Html lang="ko">
+      <Html>
         <Head>
           <link
             rel="stylesheet"
@@ -26,4 +51,4 @@ class WebAppDocument extends Document {
   }
 }
 
-export default WebAppDocument
+export default MyDocument
