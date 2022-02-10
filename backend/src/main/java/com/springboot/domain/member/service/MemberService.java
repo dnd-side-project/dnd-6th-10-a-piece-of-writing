2098f168a901c4df216923cf67273590cdb3b580
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-11 02:39:43 +0900

    [refactor] 의존 관계 제거
    Member Entity와 Security의 의존 관계 제거

--- backend/src/main/java/com/springboot/domain/member/service/MemberService.java
+++ backend/src/main/java/com/springboot/domain/member/service/MemberService.java
@@ -1,56 +1,22 @@
 package com.springboot.domain.member.service;
 
-import com.springboot.domain.common.error.exception.EntityNotFoundException;
-import com.springboot.domain.common.error.exception.ErrorCode;
-import java.util.Optional;
+import com.springboot.domain.auth.model.UserDetailsImpl;
 import javax.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
 import com.springboot.domain.member.model.Member;
-import com.springboot.domain.member.repository.MemberRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class MemberService implements UserDetailsService {
+public interface MemberService {
+    public Member findMemberByEmail(String email);
 
-    private final MemberRepository memberRepository;
+    public Member findMemberByNickname(String nickname);
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws EntityNotFoundException {
-        return memberRepository.findMemberByEmail(email);
-    }
+    public void deleteMemberByEmail(Member member);
 
-    public Member findMemberByEmail(String email) {
-        return memberRepository.findMemberByEmail(email);
-    }
-
-    public Member findMemberByNickname(String nickname) {
-        return memberRepository.findMemberByNickname(nickname);
-    }
-
-    public void deleteMemberByEmail(Member member) {
-        memberRepository.delete(member);
-    }
-
-    public Member save(Member member) {
-        return memberRepository.save(member);
-    }
+    public Member save(Member member);
 
     @Transactional
-    public void modMemberNicknameByUserEmail(Member member, String nickname) {
-        Member oldMember = memberRepository.findMemberByEmail(member.getEmail());
-        System.out.println("--!--");
-        System.out.println(nickname);
-        if(oldMember != null) {
-            oldMember.setNickname(nickname);
-        }
-    }
+    public void modMemberNicknameByUserEmail(UserDetailsImpl userDetailsImpl, String nickname);
 
-    public boolean checkNicknameDuplicate(String nickname) {
-        Member member = findMemberByNickname(nickname);
-        if(member == null) return false;
-        else return true;
-    }
+    public boolean checkNicknameDuplicate(String nickname);
+
+    public boolean checkEmailDuplicate(String email);
 }
