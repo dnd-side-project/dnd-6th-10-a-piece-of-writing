4be2facd061c48c6fc2ac95a79a04594edbb3fe0
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-12 05:12:43 +0900

    [refactor] 코드 리팩토링
    구글 스타일 가이드 적용 + Lombok 활용

--- backend/src/main/java/com/springboot/domain/posts/model/BaseTimeEntity.java
+++ backend/src/main/java/com/springboot/domain/posts/model/BaseTimeEntity.java
@@ -19,5 +19,4 @@ public class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
-
 }
--- backend/src/test/java/com/springboot/domain/posts/PostsRepositoryTest.java
+++ backend/src/test/java/com/springboot/domain/posts/PostsRepositoryTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
