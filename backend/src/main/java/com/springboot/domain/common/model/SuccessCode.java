96dff80fffb42a31e6c33fee1d02157d4aa0def4
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-17 16:22:24 +0900

    [chore] 응답 코드 수정

--- backend/src/main/java/com/springboot/domain/common/model/SuccessCode.java
+++ backend/src/main/java/com/springboot/domain/common/model/SuccessCode.java
@@ -10,18 +10,18 @@ import lombok.RequiredArgsConstructor;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum SuccessCode {
 //    Auth
-    LOGIN_SUCCESS("login success", "SA001", 200),
-    LOGOUT_SUCCESS("logout success", "SA002", 200),
-    REISSUE_SUCCESS("reissue access token success", "SA003", 200),
-    EMAIL_DUPLICATION_CHECK_SUCCESS("email is available", "SA004", 200),
-    SIGN_SUCCESS("sign-in success", "SA005", 200),
-    WITHDRAWAL_SUCCESS("withdrawal success", "SA006", 200),
+    LOGIN_SUCCESS("로그인 성공", "SA001", 200),
+    LOGOUT_SUCCESS("로그아웃 성공", "SA002", 200),
+    REISSUE_SUCCESS("access 토큰 재발급 성공", "SA003", 200),
+    EMAIL_DUPLICATION_CHECK_SUCCESS("이메일이 사용 가능합니다", "SA004", 200),
+    SIGN_SUCCESS("회원가입 성공", "SA005", 200),
+    WITHDRAWAL_SUCCESS("회원탈퇴 성공", "SA006", 200),
 
 //    Member
-    MOD_NICKNAME_SUCCESS("modify nickname success", "SM001", 200),
+    MOD_NICKNAME_SUCCESS("닉네임 설정 성공", "SM001", 200),
 
 //    Posts
-    EXTRACT_SUCCESS("Extract words success", "PM001", 200),
+    EXTRACT_SUCCESS("텍스트 추출 성공", "PM001", 200),
     SAVE_POSTS_SUCCESS("게시물 저장 성공","PM002",200),
     DELETE_POSTS_SUCCESS("게시물 삭제 성공","PM003",200),
     SELECT_ALL_POSTS_SUCCESS("모든 게시물 조회","PM004",200),
