f92330528b6a159709ce8dfefdb626c33732039f
서상혁 <ddrrpg@naver.com>
2022-02-18 22:06:52 +0900

    [feat] 팔로우 버튼 구현

--- frontend/components/button/CheckButton.tsx
+++ frontend/components/button/CheckButton.tsx
@@ -4,17 +4,56 @@ import Image from 'next/image'
 
 import { IconContainer } from '@/components/container/IconContainer'
 
+export enum CheckButtonColor {
+  WHITE = 'WHITE',
+  BLACK = 'BLACK',
+  ORANGE = 'ORANGE',
+}
+
 type Props = {
   width?: number
   height?: number
+  color?: CheckButtonColor
+  transparent?: boolean
+  noContainer?: boolean
   containerWidth?: string
   containerHeight?: string
 }
 
-const CheckButton: React.FC<Props> = ({ width = 24, height = 24, containerWidth, containerHeight }) => {
+const CheckButton: React.FC<Props> = ({
+  width = 24,
+  height = 24,
+  color = CheckButtonColor.BLACK,
+  noContainer = false,
+  transparent = false,
+  containerWidth,
+  containerHeight,
+}) => {
+  if (noContainer)
+    return (
+      <>
+        {color === CheckButtonColor.BLACK && (
+          <Image src={'/post_check.svg'} width={width} height={height} alt={'check'} />
+        )}
+        {color === CheckButtonColor.WHITE && (
+          <Image src={'/post_check_white.svg'} width={width} height={height} alt={'check'} />
+        )}
+        {color === CheckButtonColor.ORANGE && (
+          <Image src={'/post_check_orange.svg'} width={width} height={height} alt={'check'} />
+        )}
+      </>
+    )
   return (
-    <IconContainer width={containerWidth} height={containerHeight}>
-      <Image src={'/post_check.svg'} width={width} height={height} alt={'check'} />
+    <IconContainer width={containerWidth} height={containerHeight} transparent={transparent}>
+      {color === CheckButtonColor.BLACK && (
+        <Image src={'/post_check.svg'} width={width} height={height} alt={'check'} />
+      )}
+      {color === CheckButtonColor.WHITE && (
+        <Image src={'/post_check_white.svg'} width={width} height={height} alt={'check'} />
+      )}
+      {color === CheckButtonColor.ORANGE && (
+        <Image src={'/post_check_orange.svg'} width={width} height={height} alt={'check'} />
+      )}
     </IconContainer>
   )
 }
