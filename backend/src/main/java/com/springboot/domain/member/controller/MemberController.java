49d60b5457b68a45a8478bf5b69e41165bfe4b08
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-20 08:32:48 +0900

    [feat] 팔로우/팔로워/프로필 조회 api 추가

--- backend/src/main/java/com/springboot/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/springboot/domain/member/controller/MemberController.java
@@ -1,16 +1,12 @@
 package com.springboot.domain.member.controller;
 
 import com.springboot.domain.auth.model.UserDetailsImpl;
-import com.springboot.domain.common.error.exception.BusinessException;
-import com.springboot.domain.common.error.exception.ErrorCode;
 import com.springboot.domain.common.model.ResponseDto;
-import com.springboot.domain.common.service.ResponseService;
-import com.springboot.domain.common.service.ResponseServiceImpl;
-import com.springboot.domain.common.model.SuccessCode;
+import com.springboot.domain.member.model.Dto.ModProfileDto;
 import com.springboot.domain.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import java.util.HashMap;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +17,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -30,6 +26,7 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/member")
 public class MemberController {
+
     private final MemberService memberService;
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER')")
@@ -38,11 +35,41 @@ public class MemberController {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
-    @Operation(summary = "nickname patch api", description = "닉네임 변경 api \nbody에 변경할 nickname을 함께 전송")
+    @Operation(summary = "nickname patch api", description = "닉네임 변경 api \npath에 nickname 필요")
     @PatchMapping(value = "/nickname/{nickname}")
     public ResponseEntity<?> modNickname(
             @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
             @Parameter(description = "닉네임", required = true) @PathVariable @NotBlank String nickname) {
-        return memberService.modMemberNicknameByUserEmail(userDetailsImpl, nickname);
+        return memberService.modNickname(userDetailsImpl, nickname);
+    }
+
+    @Operation(summary = "profile change api", description = "프로필 사진 변경 api")
+    @PostMapping(value = "/profile")
+    public ResponseEntity<?> modProfile(
+            @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+            @Valid ModProfileDto modProfileDto) {
+        return memberService.modProfile(modProfileDto, userDetailsImpl);
+    }
+
+    @Operation(summary = "get profile api", description = "프로필 조회 api")
+    @GetMapping(value = "/profile/{nickname}")
+    public ResponseEntity<?> getMemberProfile(
+            @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+            @Parameter(description = "닉네임", required = true) @PathVariable @NotBlank String nickname) {
+        return memberService.getMemberProfile(userDetailsImpl, nickname);
+    }
+
+    @Operation(summary = "팔로우 목록 api", description = "팔로우 목록 조회 api, path에 nickname 필요")
+    @GetMapping(value = "/follow/list/{nickname}")
+    public ResponseEntity<? extends ResponseDto> getFollowList(
+            @Parameter(description = "닉네임", required = true) @PathVariable String nickname) {
+        return memberService.getFollowList(nickname);
+    }
+
+    @Operation(summary = "팔로워 목록 api", description = "팔로워 목록 조회 api, path에  nickname 필요")
+    @GetMapping(value = "/follower/list/{nickname}")
+    public ResponseEntity<? extends ResponseDto> getFollowerList(
+            @Parameter(description = "닉네임", required = true) @PathVariable String nickname) {
+        return memberService.getFollowerList(nickname);
     }
 }
