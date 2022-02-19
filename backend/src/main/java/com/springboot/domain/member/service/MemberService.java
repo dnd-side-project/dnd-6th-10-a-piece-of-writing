c39662d5dd90dc6aa51aed91b7762645397af4a8
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-20 08:34:20 +0900

    [feat] 팔로워/팔로우/프로필 조회 서비스 코드 추가

--- backend/src/main/java/com/springboot/domain/member/service/MemberService.java
+++ backend/src/main/java/com/springboot/domain/member/service/MemberService.java
@@ -2,11 +2,14 @@ package com.springboot.domain.member.service;
 
 import com.springboot.domain.auth.model.UserDetailsImpl;
 import com.springboot.domain.common.model.ResponseDto;
-import javax.transaction.Transactional;
+import com.springboot.domain.member.model.Dto.ModProfileDto;
 import com.springboot.domain.member.model.Member;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
+    public Member findMemberById(Long id);
+
     public Member findMemberByEmail(String email);
 
     public Member findMemberByNickname(String nickname);
@@ -15,9 +18,21 @@ public interface MemberService {
 
     public Member save(Member member);
 
-    public ResponseEntity<? extends ResponseDto> modMemberNicknameByUserEmail(UserDetailsImpl userDetailsImpl, String nickname);
+    public ResponseEntity<? extends ResponseDto> modNickname(UserDetailsImpl userDetailsImpl,
+            String nickname);
 
     public ResponseEntity<? extends ResponseDto> checkNicknameDuplicate(String nickname);
 
     public ResponseEntity<? extends ResponseDto> checkEmailDuplicate(String email);
+
+    public String profileImgUpload(MultipartFile multipartFile, String id);
+
+    public ResponseEntity<? extends ResponseDto> modProfile(ModProfileDto modProfileDto,
+            UserDetailsImpl userDetailsImpl);
+
+    public ResponseEntity<? extends ResponseDto> getMemberProfile(UserDetailsImpl userDetailsImpl, String nickname);
+
+    public ResponseEntity<? extends ResponseDto> getFollowList(String nickname);
+
+    public ResponseEntity<? extends ResponseDto> getFollowerList(String nickname);
 }
