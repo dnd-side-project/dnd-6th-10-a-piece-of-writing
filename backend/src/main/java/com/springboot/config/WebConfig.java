c72a7d50eb2d14c453f8a9eb9ac16b950bc0036a
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-02-22 01:19:44 +0900

    [feat] cors 설정 변경

--- backend/src/main/java/com/springboot/config/WebConfig.java
+++ backend/src/main/java/com/springboot/config/WebConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*").allowedMethods("GET", "POST", "PATCH");
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3333", "http://pieceofwriting.kro.kr/")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
