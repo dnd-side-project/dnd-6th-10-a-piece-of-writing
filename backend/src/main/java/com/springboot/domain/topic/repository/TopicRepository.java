0de2d0044a0dd13de43404fd266308d33b4240c5
carnival77 <stam0325@gmail.com>
2022-02-23 23:15:22 +0900

    [feat] topic 생성 기능 코드 및 테스트 코드 구현 #52

--- backend/src/main/java/com/springboot/domain/topic/repository/TopicRepository.java
+++ backend/src/main/java/com/springboot/domain/topic/repository/TopicRepository.java
@@ -1,11 +1,14 @@
 package com.springboot.domain.topic.repository;
 
+import com.springboot.domain.reply.model.entity.Reply;
 import com.springboot.domain.topic.model.entity.Topic;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    List<Topic> findAllByOrderByIdDesc();
 
     // 입력하는 name이 기존 토픽 데이터테이블에 있는지 확인하고, 일치하는 내역을 목록(topic_id, name)으로 보낸다.
 //    @Query(value="select * from Topic t "
